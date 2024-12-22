ee87b6de9f206db94f4b7f8a2492d30edc1d91c9
Lisa <115975403+lschiedel02@users.noreply.github.com>
2024-12-22 16:25:44 +0000

    cloud run works again, adding open telematry now

--- note_api/main.py
+++ note_api/main.py
@@ -1,24 +1,41 @@
-# -*- coding: utf-8 -*-
+import os
+from fastapi import FastAPI, Depends
 from uuid import uuid4
 from typing import List, Optional
-from os import getenv
 from typing_extensions import Annotated
-
-from fastapi import Depends, FastAPI
 from starlette.responses import RedirectResponse
+from opentelemetry import trace
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from .backends import Backend, RedisBackend, MemoryBackend, GCSBackend
 from .model import Note, CreateNoteRequest
 
+# Initialize OpenTelemetry Tracer provider and exporter
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "note-api"})
+    )
+)
+tracer_provider = trace.get_tracer_provider()
+cloud_exporter = CloudTraceSpanExporter()
+span_processor = BatchExportSpanProcessor(cloud_exporter)
+tracer_provider.add_span_processor(span_processor)
+
+# Create the FastAPI app
 app = FastAPI()
+
+# Instrument FastAPI app
+FastAPIInstrumentor.instrument_app(app)
 
 my_backend: Optional[Backend] = None
 
-
 def get_backend() -> Backend:
-    global my_backend  # pylint: disable=global-statement
+    global my_backend
     if my_backend is None:
-        backend_type = getenv('BACKEND', 'memory')
-        print(backend_type)
+        backend_type = os.getenv('BACKEND', 'memory')
         if backend_type == 'redis':
             my_backend = RedisBackend()
         elif backend_type == 'gcs':
@@ -27,38 +44,26 @@ def get_backend() -> Backend:
             my_backend = MemoryBackend()
     return my_backend
 
-
 @app.get('/')
 def redirect_to_notes() -> None:
     return RedirectResponse(url='/notes')
 
-
 @app.get('/notes')
 def get_notes(backend: Annotated[Backend, Depends(get_backend)]) -> List[Note]:
     keys = backend.keys()
-
-    Notes = []
-    for key in keys:
-        Notes.append(backend.get(key))
+    Notes = [backend.get(key) for key in keys]
     return Notes
 
-
 @app.get('/notes/{note_id}')
-def get_note(note_id: str,
-             backend: Annotated[Backend, Depends(get_backend)]) -> Note:
+def get_note(note_id: str, backend: Annotated[Backend, Depends(get_backend)]) -> Note:
     return backend.get(note_id)
 
-
 @app.put('/notes/{note_id}')
-def update_note(note_id: str,
-                request: CreateNoteRequest,
-                backend: Annotated[Backend, Depends(get_backend)]) -> None:
+def update_note(note_id: str, request: CreateNoteRequest, backend: Annotated[Backend, Depends(get_backend)]) -> None:
     backend.set(note_id, request)
 
-
 @app.post('/notes')
-def create_note(request: CreateNoteRequest,
-                backend: Annotated[Backend, Depends(get_backend)]) -> str:
+def create_note(request: CreateNoteRequest, backend: Annotated[Backend, Depends(get_backend)]) -> str:
     note_id = str(uuid4())
     backend.set(note_id, request)
     return note_id
